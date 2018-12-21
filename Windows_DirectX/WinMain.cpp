652fffea0ffa342a11a8264314be2349bf0302da
Cavan Biggs <cbiggs7635@conestogac.on.ca>
2018-12-21 00:04:28 -0500

    WinMain windows created

---
+++ Windows_DirectX/WinMain.cpp
@@ -0,0 +1,16 @@
+
+
+
+#include <windows.h>
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	LPCSTR greeting = "Hello World!";
+	LPCSTR description = "Windows Progrmming API";
+
+	MessageBox(NULL, greeting, description, MB_ICONHAND | MB_OK);
+
+
+	return 0;
+}
